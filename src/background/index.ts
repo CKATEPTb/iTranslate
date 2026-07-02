0b52f55536c6d1e4a9f8e93ba0e0bdd3fde95c33
CKATEPTb <ckateptb@vivaldi.net>
2026-07-02 23:33:02 +0200

    fix: reset manual page translation on reload

--- src/background/index.ts
+++ src/background/index.ts
@@ -419,6 +419,12 @@ chrome.tabs.onRemoved.addListener((tabId) => {
   void pageTranslationSessions.clear(tabId)
 })
 
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.status === 'loading') {
+    void pageTranslationSessions.clear(tabId)
+  }
+})
+
 installPageTranslationContextMenu(startPageTranslationFromContextMenu, getPageTranslationTargetLanguage)
 
 // command handler
